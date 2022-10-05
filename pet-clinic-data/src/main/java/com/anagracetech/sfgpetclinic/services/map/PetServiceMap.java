c300d6077698f9c448370b279409790fd6596e97
Chima Anason <tblynxx@gmail.com>
2022-10-06 01:05:13 +0800

    Adding bootstrap for data. Closes #15.

--- pet-clinic-data/src/main/java/com/anagracetech/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/anagracetech/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package com.anagracetech.sfgpetclinic.services.map;
 
 import com.anagracetech.sfgpetclinic.model.Pet;
 import com.anagracetech.sfgpetclinic.services.CrudService;
+import com.anagracetech.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/anagracetech/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/anagracetech/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.anagracetech.sfgpetclinic.services.map;
 
 import com.anagracetech.sfgpetclinic.model.Vet;
-import com.anagracetech.sfgpetclinic.services.CrudService;
+import com.anagracetech.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
