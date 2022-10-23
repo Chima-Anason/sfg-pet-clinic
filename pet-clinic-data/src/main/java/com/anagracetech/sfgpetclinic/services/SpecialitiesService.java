0ba817a59e39ee334439cd8add33fccbc0df8971
Chima Anason <tblynxx@gmail.com>
2022-10-23 23:38:47 +0800

    added Specialities Impl. Closes #32.

---
+++ pet-clinic-data/src/main/java/com/anagracetech/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.anagracetech.sfgpetclinic.services;
+
+import com.anagracetech.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
